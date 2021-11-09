0a2d0898e2310ea09d9ab3ec21340b803950c620
clicmdscript <93974658+clicmdscript@users.noreply.github.com>
2021-11-09 19:40:14 +0700

    Update install_awscli.sh

--- install_awscli.sh
+++ install_awscli.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install software-properties-common
-yes '' |sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.8
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf awscliv2.zip
-
-#require plugin JQ
-sudo apt install -y jq
-
+cd /home/ubuntu
 #wget all file 
 wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/ap-northeast-1.sh
 wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/ap-northeast-2.sh
@@ -29,11 +17,25 @@ wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/us-west-2.sh
 wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/add_cronjob.sh
 wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/cron.sh
 wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/ec2delete.sh
-wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/install_awscli.sh
+#wget https://raw.githubusercontent.com/clicmdscript/easycmd/main/install_awscli.sh
 
 chmod +x *.sh
 echo "Add cron to system"
 ./add_cronjob.sh
 
-echo "done"
+echo "Install Python 3.8"
+sudo apt update
+sudo apt install software-properties-common
+yes '' |sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.8
+echo "Install AWS-CLI"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip
+
+echo "Install JQ addon"
+sudo apt install -y jq
+
 echo "Install Completed"
+
