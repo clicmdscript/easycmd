14cad676c97949b8053eec74c4612eb38a09d490
Jame <lungtungner@gmail.com>
2021-11-15 00:25:33 +0700

    Update _first_run.sh

--- _first_run.sh
+++ _first_run.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
-    cd /home/ubuntu
-    git clone https://github.com/clicmdscript/easycmd.git
-    cd easycmd/
-    mv *.sh /home/ubuntu
-    cd ..
-    rm -rf easycmd/
-    chmod +x *.sh
-    ./install_awscli.sh
-    
+ 
+rm -rf ippool/
+rm -rf keypem
+rm -rf log/
+rm -rf *.sh
+rm -rf cronjobgen*
+rm -rf *.json
+rm -rf smtp-cli
+rm -rf temgroup.txt
+rm -rf tempsubnet.txt
 
-n=1
-while [ $n -lt 5 ]
-do
-        current_time=$(date "+%Y.%m.%d-%H.%M")
-        echo "RunTime now = $n"
-        ./cron.sh > log/$current_time-runscreen.txt
-sleep 100000s
-        echo "Done"
-            ((n=$n+1))
-done
+git clone https://github.com/clicmdscript/easycmd.git
+mv easycmd/* /home/ubuntu/
+rm -rf easycmd/
 
+chmod +x mail/*.sh
+chmod +x mail/smtp-cli
+chmod +x *.sh
+chmod +x includes/*.sh
+mkdir log/
+echo "NEW install" > log/log_addcron.txt
+mkdir keypem/
+mkdir ippool/
+mkdir temp/
 
+./_install_awscli.sh
 
-# get special line in file
-sed -n '2,2p' file.txt
-
-# count number line in file
-cat -n file01.txt | tail -1 | awk '{print $1}'
-
-
-sec_number_a=$(cat -n temgroup.txt | tail -1 | awk '{print $1}')
-sec_number_b=2
-
-if [ "$sec_number_a" -ge "$sec_number_b" ]
-     then
-        echo       "create subnet"
-     else
-        echo       "create ec2"
-fi
